974ecbb509f2d3032c1e1cefc20d4ff37089a1c0
U Sai Nath Rao <87133044+usai729@users.noreply.github.com>
2022-03-31 19:02:51 +0530

    removed password

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect("localhost", "root", "rootmysql@1#", "simple_img_posts");
+    $conn = mysqli_connect("localhost", "root", "************", "simple_img_posts");
 
     if (!$conn) {
         die("Couldn't connect to database");
